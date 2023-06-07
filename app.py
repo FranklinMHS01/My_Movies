cb3de1243ce83284648acba1006ff7681809bb58
FranklinMHS01 <116238974+FranklinMHS01@users.noreply.github.com>
2023-06-07 12:33:35 +0700

    app.py

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def movie_get():
     return jsonify({'movies': movie_list})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=100, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
